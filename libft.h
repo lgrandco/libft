f8edcaa4fe0e021119fa79ff05ef94df045009b5
Leo Grandcoin <legrandc@bess-f3r3s3.clusters.42paris.fr>
2023-10-30 18:34:23 +0100

    z

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: legrandc <legrandc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 17:55:36 by legrandc          #+#    #+#             */
+/*   Updated: 2023/10/30 18:16:00 by legrandc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	isalpha(int c);
+int	isdigit(int c);
+
+#endif
